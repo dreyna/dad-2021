e4c60235e6910a0a5760ba75493d5bc36b57315e
dreyna <reyna_b@upeu.edu.pe>
2021-04-13 16:53:20 -0500

    sockets

---
+++ cliente.py
@@ -0,0 +1,25 @@
+import socket
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.2"
+ADDR = (SERVER, PORT)
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    cliente.send(send_length)
+    cliente.send(message)
+    print(cliente.recv(2048).decode(FORMAT))
+send("Distribuidas")
+input()
+send("¿Cómo estas...?")
+input()
+send("Muy bien")
+
